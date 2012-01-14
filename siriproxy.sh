707fff93e256b2ce71790183acff904075e2b364
Jason Bean <jbeantx@gmail.com>
2012-01-13 20:20:17 -0600

    Update siriproxy.sh

--- siriproxy.sh
+++ siriproxy.sh
@@ -25,10 +25,12 @@
 START="rvmsudo siriproxy server 1>> ~/.siriproxy/debug.log 2>> ~/.siriproxy/error.log &"
 
 case $1 in
+
 start)
 $START
 exit 0
 ;;
+
 stop)
 echo "Stopping..."
 sudo killall -w -g sleep > /dev/null
@@ -36,28 +38,31 @@ sudo killall siriproxy > /dev/null
 echo "Done."
 exit 0
 ;;
+
 # all code by methoddk
 keepalive)
 while true; do
-	if ps ax | grep -v grep | grep "$HOME/.rvm/gems/ruby-1.9.3-p0@SiriProxy/bin/" > /dev/null
-	then
-		echo $(date) "SiriProxy is running!"
-	else
-		echo $(date) "SiriProxy is not running. Restarting..."
-		${START}
-		sleep 5
-		if ps ax | grep -v grep | grep "$HOME/.rvm/gems/ruby-1.9.3-p0@SiriProxy/bin/" > /dev/null
-			then
-				echo $(date) "SiriProxy is running!"
-			else
-				echo $(date) "Something is wrong."
-		fi
-	fi
-	sleep 600
+  if ps ax | grep -v grep | grep "$HOME/.rvm/gems/ruby-1.9.3-p0@SiriProxy/bin/" > /dev/null
+    then
+      echo $(date) "SiriProxy is running!"
+    else
+      echo $(date) "SiriProxy is not running. Restarting..."
+      ${START}
+      sleep 5
+      if ps ax | grep -v grep | grep "$HOME/.rvm/gems/ruby-1.9.3-p0@SiriProxy/bin/" > /dev/null
+        then
+          echo $(date) "SiriProxy is running!"
+        else
+          echo $(date) "Something is wrong."
+      fi
+  fi
+  sleep 600
 done
 ;;
+
 *)
 echo "Usage: rvmsudo sh $0 {start|keepalive|stop}"
 exit 0
 ;;
+
 esac
